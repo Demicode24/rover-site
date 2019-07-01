12b9698c60fd8a69475de12fbd85919981f2715c
Dave Guillaume <me@daveg.com>
2019-07-01 10:09:51 -0400

    proved

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+$(function () {
+  $('[data-id=headers-4-sidebar-toggle]').click(function (e) {
+    e.preventDefault();
+    $('#headers-4-sidebar').toggleClass('show');
+  });
+  $('[data-id=headers-4-sidebar-close]').click(function (e) {
+    e.preventDefault();
+    $('#headers-4-sidebar').removeClass('show');
+  });
+});
